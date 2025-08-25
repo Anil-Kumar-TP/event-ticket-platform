2aafd7be1aebe1fce2d0553cfaafb0860e1af2f3
Anil <anil6996f@gmail.com>
2025-08-25 13:58:15 +0530

    implemented search functionality for listing published events

--- src/main/java/com/anil/tickets/controllers/PublishedEventController.java
+++ src/main/java/com/anil/tickets/controllers/PublishedEventController.java
@@ -1,6 +1,7 @@
 package com.anil.tickets.controllers;
 
 import com.anil.tickets.domain.dtos.ListPublishedEventResponseDto;
+import com.anil.tickets.domain.entities.Event;
 import com.anil.tickets.mappers.EventMapper;
 import com.anil.tickets.services.EventService;
 import jakarta.el.ELManager;
@@ -10,6 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,8 +23,13 @@ public class PublishedEventController {
     private final EventMapper eventMapper;
 
     @GetMapping
-    public ResponseEntity<Page<ListPublishedEventResponseDto>> listPublishedEvents(Pageable pageable){
-        return ResponseEntity.ok(eventService.listPublishedEvents(pageable)
-                .map(eventMapper::toListPublishedEventResponseDto));
+    public ResponseEntity<Page<ListPublishedEventResponseDto>> listPublishedEvents(@RequestParam(required = false) String q, Pageable pageable){
+        Page<Event> events;
+        if (q != null && !q.trim().isEmpty()){
+            events = eventService.searchPublishedEvents(q,pageable);
+        }else {
+            events = eventService.listPublishedEvents(pageable);
+        }
+        return ResponseEntity.ok(events.map(eventMapper::toListPublishedEventResponseDto));
     }
 }
